c6208a0d6dbee9aaedd1d7fc7fedde0d37c1a72b
amiedawn <amie.d.carroll@gmail.com>
2020-10-09 20:38:44 -0600

    length lower and upper work

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -12,9 +11,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
-debugger;
+
 // Print password in box
 function generatePassword() {
+debugger;
   //*******************start length*************** */
   // prompt: choose length of password between 8-128 characters, default 8
   var chosenLength = window.prompt("How many characters would you like in your password?", "8");
@@ -29,147 +29,83 @@ function generatePassword() {
 
   // validate that chosenLength is between 8 and 128
   if (chosenLength < 8 || chosenLength > 128) {
-    window.alert("Please enter a number between 8 and 128.");
+    window.alert("Please start over and enter a number between 8 and 128.");
     // stops and doesn't print anything in the box
     return "";
   }
   // label, # of characters, and variable type shows in log
   console.log('chosenLength', chosenLength, typeof chosenLength);
 
-  return 0;
-  //  convert string to a number
+  // go to prompts to see what criteria they want
+  decideCharTypes();
 }
 //*************end of length******************** */
 
-//************start lowercase******************* */
+
 // TODO: prompt: choose if there will be lowercase?, uppercase?, numeric? and/or special characters? 
-// valid values are arrays
+//   valid values are arrays
 // WHEN prompted for character types to include in the password
 // THEN I choose lowercase, uppercase, numeric, and / or special characters
-debugger;
-function randomLetter() {
-  var yesLowercase = window.prompt("Would you like to include at least 1 lowercase letter?");
+
+//************start lowercase******************* */
+function decideCharTypes() {
+  debugger;
+  var yesLowercase = window.prompt("Would you like to include at least 1 lowercase letter? \nPlease type 'y' for yes or 'n' for no.");
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var lowercaseLetter = lowercase[Math.floor(Math.random() * lowercase.length)];
 
   // valid answers are 'yes' or 'no' and convert string to boolean
-  if (yesLowercase === "yes") {
+  if (yesLowercase === "y") {
     yesLowercase = true;
-    return lowercaseLetter;
+    passwordText = lowercaseLetter;
+    //save 1st character of password
+    passwordText[0] = passwordText;
+
     //return passwordText.value;
-  } else if (yesLowercase === "no") {
+  } else if (yesLowercase === "n") {
     yesLowercase = false;
   } else {
-    alert("Please enter 'yes' or 'no'.");
-    return "";
-
-    console.log('yesLowercase', yesLowercase, typeof yesLowercase);
-  }
-
-  //**add array for adding in lowercase letters **/
-
+    window.confirm("Invalid entry. Try again.");
+    decideCharTypes();
+  }      
+  console.log('lowercaseLetter', lowercaseLetter, typeof lowercaseLetter);
+  console.log('passwordText', passwordText, typeof passwordText);
   //************end lowercase********************* */
 
   //************start uppercase******************* */
-  // TODO: prompt: choose if there will be lowercase?, uppercase?, numeric? and/or special characters? 
-  // valid values are arrays
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and / or special characters
-  var yesUppercase = window.prompt("Would you like to include at least 1 uppercase letter?");
+  var yesUppercase = window.prompt("Would you like to include at least 1 uppercase letter? \nPlease type 'y' for yes or 'n' for no.");
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var uppercaseLetter = uppercase[Math.floor(Math.random() * uppercase.length)];
+
   // valid answers are 'yes' or 'no' and convert string to boolean
-  if (yesUppercase === "yes") {
+  if (yesUppercase === "y") {
     yesUppercase = true;
-  } else if (yesUppercase === "no") {
+    if (yesLowercase = true) {
+      passwordText = passwordText[0] + uppercaseLetter;
+    } else {
+      passwordText[0] = uppercaseLetter;
+    }  
+  } else if (yesUppercase === "n") {
     yesUppercase = false;
   } else {
-    alert("Please enter 'yes' or 'no'.");
-    return "";
-  }
-  console.log('yesUppercase', yesUppercase, typeof yesUppercase);
+    window.prompt("Invalid entry. Please start over.");
+    decideCharTypes();
+  }  
 
-  //**add array for adding in uppercase letters **/
-
+    console.log('yesUppercase', yesUppercase, typeof yesUppercase);
+    console.log('passwordText', passwordText, typeof passwordText);
   //************end uppercase********************* */
 
-  //************start numeric******************* */
-  // TODO: prompt: choose if there will be lowercase?, uppercase?, numeric? and/or special characters? 
-  // valid values are arrays
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and / or special characters
-  var yesNumeric = window.prompt("Would you like to include at least 1 number?");
-  // valid answers are 'yes' or 'no' and convert string to boolean
-  if (yesNumeric === "yes") {
-    yesNumeric = true;
-  } else if (yesNumeric === "no") {
-    yesNumeric = false;
-  } else {
-    alert("Please enter 'yes' or 'no'.");
-    return "";
-  }
-  console.log('yesNumeric', yesNumeric, typeof yesNumeric);
-
-  //**add array for adding in numbers **/
-
-  //************end numeric********************* */
-
-  //************start special characters******************* */
-  // TODO: prompt: choose if there will be lowercase?, uppercase?, numeric? and/or special characters? 
-  // valid values are arrays
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and / or special characters
 
 
-  // ***NOT CHANGING YET BECAUSE NOT SURE HOW
-  // var yesNumeric = window.prompt("Would you like to include at least 1 number?");
-  // // valid answers are 'yes' or 'no' and convert string to boolean
-  // if (yesNumeric === "yes") {
-  //   yesNumeric = true;
-  // } else if (yesNumeric === "no") {
-  //   yesNumeric = false;
-  // } else {
-  //   alert("Please enter 'yes' or 'no'.");
-  //   return "";
-  // }
-  // console.log('yesNumeric', yesNumeric, typeof yesNumeric);
-
-  //**add array?? for adding in for special characters **/
-
-  //************end special characters********************* */
-
-  var allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-  var Uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialChars;
-
-
-
-
-  //generate a number:
-  //  chosenLength:  8 -> 45984938, 128 -> (128 digit number)
-  //    yesLowercase: array[i] where i = every lowercase letter
-  //    yesUppercase: array[i] where i = every uppercase letter
-  //    yesNumeric: array[i] where i = any digit 0-9
-  //    yesSpecialCharacter??: anything else
-
-  //  display the output in the box
-
-  // ***NOT SURE IF THIS WORKS******
-  // debugger;
-  // var passwordCharacter = "";  // each character in the password
-  // var showChar = password[i];
-  // for (var i = 0; i < showChar; i = i + 1) {
-  //   var allIndex = Math.floor(Math.random() * allNumbers.length);
-  //   showChar = showChar + allNumbers[allIndex];
-
-
-  // }
-
-
-
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-
-  return 0;
 }
+
+
+// // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//go to randomUpLetter
+//randomUpLetter();
+
+
+
