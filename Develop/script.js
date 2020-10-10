b9f931c4124f76bc0c937dabe779d6fd454293f2
amiedawn <amie.d.carroll@gmail.com>
2020-10-10 16:23:23 -0600

    done except loops through twice and won't display

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,47 @@
 // Assignment code here
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+ 
+  
   passwordText.value = password;
 
-}
+  // go to prompts to see what criteria they want
+  generatePassword();
+
+};
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // Print password in box
 function generatePassword() {
+
+  //************ lowercase array ******************* */
+  var yesLowercase = window.confirm("Would you like to include at least 1 lowercase letter? \nClick 'OK' for yes and 'Cancel for no.");
+  var arrLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var lowercaseLetter = arrLowercase[Math.floor(Math.random() * arrLowercase.length)];
+
+  //************ uppercase array ******************* */
+  var yesUppercase = window.confirm("Would you like to include at least 1 uppercase letter? \nClick 'OK' for yes and 'Cancel for no.");
+  var arrUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var uppercaseLetter = arrUppercase[Math.floor(Math.random() * arrUppercase.length)];
+
+  //************ number array ******************* */
+  var yesNumber = window.confirm("Would you like to include at least 1 number? \nClick 'OK' for yes and 'Cancel for no.");
+  var arrNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var numberChar = arrNumber[Math.floor(Math.random() * arrNumber.length)];
+
+  //************ special array ******************* */
+  var yesSpecial = window.confirm("Would you like to include any special characters? \nClick 'OK' for yes and 'Cancel for no.");
+  var arrSpecial = ["~", "!", "#", "$", "%", "^", "&", "*", "+", "=", "-", "?"];
+  var specialChar = arrSpecial[Math.floor(Math.random() * arrSpecial.length)];
 debugger;
-  //*******************start length*************** */
+  //*******************start ask length*************** */
   // prompt: choose length of password between 8-128 characters, default 8
   var chosenLength = window.prompt("How many characters would you like in your password?", "8");
   // convert string to number, base 10
@@ -23,7 +49,7 @@ debugger;
 
   // validate that chosenLength is a number
   if (Number.isNaN(chosenLength)) {
-    alert("Your entry is not a number. Please enter a number between 8 and 128.");
+    alert("Your entry is not a number. Please start over and enter a number between 8 and 128.");
     return "";
   }
 
@@ -35,77 +61,56 @@ debugger;
   }
   // label, # of characters, and variable type shows in log
   console.log('chosenLength', chosenLength, typeof chosenLength);
+  //*************end of ask length******************** */
 
-  // go to prompts to see what criteria they want
-  decideCharTypes();
-}
-//*************end of length******************** */
+  // initialize empty password string and characterset array
+  password = "";
+  var passwordArrAll = [];
 
+  //************start collect all possible characters to include in password***** */
+  // if user wants lowercase, include it
+  if (yesLowercase = true) {
+    passwordArrAll = passwordArrAll.concat(arrLowercase);
+    var lowerIndex = Math.floor(Math.random() * arrLowercase.length);
+    password = password + arrLowercase[lowerIndex];
+    console.log('passwordArrAll', passwordArrAll, typeof passwordArrAll);
+  }
+  
+  // if user wants uppercase, include and concat it
+  if (yesUppercase = true)  {
+    passwordArrAll = passwordArrAll.concat(arrUppercase); 
+    var upperIndex = Math.floor(Math.random() * arrUppercase.length);
+    password = password + arrUppercase[upperIndex];
+    console.log('passwordArrAll', passwordArrAll, typeof passwordArrAll);
+  }
 
-// TODO: prompt: choose if there will be lowercase?, uppercase?, numeric? and/or special characters? 
-//   valid values are arrays
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and / or special characters
+  // if user wants a number, include and concat it
+  if (yesNumber = true) {
+    passwordArrAll = passwordArrAll.concat(arrNumber);
+    var numberIndex = Math.floor(Math.random() * arrNumber.length);
+    password = password + arrNumber[numberIndex];
+    console.log('passwordArrAll', passwordArrAll, typeof passwordArrAll);
+  }
 
-//************start lowercase******************* */
-function decideCharTypes() {
-  debugger;
-  var yesLowercase = window.prompt("Would you like to include at least 1 lowercase letter? \nPlease type 'y' for yes or 'n' for no.");
-  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var lowercaseLetter = lowercase[Math.floor(Math.random() * lowercase.length)];
-
-  // valid answers are 'yes' or 'no' and convert string to boolean
-  if (yesLowercase === "y") {
-    yesLowercase = true;
-    passwordText = lowercaseLetter;
-    //save 1st character of password
-    passwordText[0] = passwordText;
-
-    //return passwordText.value;
-  } else if (yesLowercase === "n") {
-    yesLowercase = false;
-  } else {
-    window.confirm("Invalid entry. Try again.");
-    decideCharTypes();
-  }      
-  console.log('lowercaseLetter', lowercaseLetter, typeof lowercaseLetter);
-  console.log('passwordText', passwordText, typeof passwordText);
-  //************end lowercase********************* */
-
-  //************start uppercase******************* */
-  var yesUppercase = window.prompt("Would you like to include at least 1 uppercase letter? \nPlease type 'y' for yes or 'n' for no.");
-  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var uppercaseLetter = uppercase[Math.floor(Math.random() * uppercase.length)];
-
-  // valid answers are 'yes' or 'no' and convert string to boolean
-  if (yesUppercase === "y") {
-    yesUppercase = true;
-    if (yesLowercase = true) {
-      passwordText = passwordText[0] + uppercaseLetter;
-    } else {
-      passwordText[0] = uppercaseLetter;
-    }  
-  } else if (yesUppercase === "n") {
-    yesUppercase = false;
-  } else {
-    window.prompt("Invalid entry. Please start over.");
-    decideCharTypes();
+  // if user wants a special character, include and concat it
+  if (yesSpecial = true) {
+    passwordArrAll = passwordArrAll.concat(arrSpecial);
+    var specialIndex = Math.floor(Math.random() * arrSpecial.length);
+    password = password + arrSpecial[specialIndex];
+    console.log('passwordArrAll', passwordArrAll, typeof passwordArrAll);
   }  
-
-    console.log('yesUppercase', yesUppercase, typeof yesUppercase);
-    console.log('passwordText', passwordText, typeof passwordText);
-  //************end uppercase********************* */
-
-
-
-}
-
-
-// // Add event listener to generate button
+  //************end collect all possible characters to include in password***** */
+  console.log('chosenLength', chosenLength, typeof chosenLength);
+  
+  // loop to get the right number of characters to match the desired length
+  for (var i = password.length; i <= (chosenLength - 1); i = i + 1) {
+    var passwordIndex = Math.floor(Math.random() * passwordArrAll.length);
+    password = password + passwordArrAll[passwordIndex];
+  }
+};
+ 
 generateBtn.addEventListener("click", writePassword);
 
-//go to randomUpLetter
-//randomUpLetter();
 
 
-
+    
